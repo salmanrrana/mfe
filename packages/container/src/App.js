d97390be0cd8ecfc11482c862bddc2f526a20915
Salman Rana <salman.r.rana@gmail.com>
2020-11-29 13:43:14 -0600

    authentication and passing state to container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
   StylesProvider,
@@ -17,14 +17,23 @@ const generateClassName = createGenerateClassName({
 });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  console.log('isSignedIn: ', isSignedIn);
+
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
+          <Header isSignedIn={isSignedIn} />
           <Suspense fallback={<Progress />}>
             <Switch>
-              <Route path='/auth' component={AuthLazy} />
+              <Route path='/auth'>
+                <AuthLazy
+                  onSignIn={() => setIsSignedIn(true)}
+                  onSignOut={() => setIsSignedIn(false)}
+                />
+              </Route>
               <Route path='/' component={MarketingLazy} />
             </Switch>
           </Suspense>
