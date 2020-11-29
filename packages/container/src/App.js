fa4033ec12df9475fbfa9cfb18b56f1c4abf000a
Salman Rana <salman.r.rana@gmail.com>
2020-11-29 13:54:20 -0600

    adjust auth state

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -25,14 +25,14 @@ export default () => {
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header isSignedIn={isSignedIn} />
+          <Header
+            onSignOut={() => setIsSignedIn(false)}
+            isSignedIn={isSignedIn}
+          />
           <Suspense fallback={<Progress />}>
             <Switch>
               <Route path='/auth'>
-                <AuthLazy
-                  onSignIn={() => setIsSignedIn(true)}
-                  onSignOut={() => setIsSignedIn(false)}
-                />
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
               <Route path='/' component={MarketingLazy} />
             </Switch>
