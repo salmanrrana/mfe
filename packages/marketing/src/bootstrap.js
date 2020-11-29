a10d60a1b8cb4b18e0671cee94fe1fe26632ab3b
Salman Rana <salman.r.rana@gmail.com>
2020-11-29 10:41:33 -0600

    (routing)container to child and child to container communication

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
 
 // mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
 };
 
 // if we are iun development and in isolation,
 // so call mount immediately
-if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_feed-dev-root');
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_feed-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
